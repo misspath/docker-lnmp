a25a428a99f0c29110fd9990d6cd347b0a48294a
Your Name <you@example.com>
2019-04-19 01:24:20 +0800

    fixed

--- free/index.php
+++ free/index.php
@@ -5,16 +5,15 @@ declare(strict_types = 1);
 
 ?>
 
-<title>docker-lnmp</title>
+<title>DOCKER-L.N.M.P</title>
 
 <?php
 
 // Declare function
 function say(string $world) : string {
-    return '<center>hello ' . $world . '</center>';
+    return '<h3 style="text-align: center;">hello ' . $world . '</h3>';
 }
 
 echo say('docker-lnmp, by leon &lt;jiangxilee@gmail.com&gt;');
 
 phpinfo();
-?>
